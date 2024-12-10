f193e28e51bc82c252cc2094e85e0c853268e8a6
gyvmc <gyvmc@10.16.3.87>
2024-12-10 17:19:33 -0500

    ICE 5 by Rudraksh Saini (Student: 991725403)

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+
+
+
+
+/**
+ *
+ * @author gyvmc
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+            System.out.println("This is Naman");
+    }
+}
